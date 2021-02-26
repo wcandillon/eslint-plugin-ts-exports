fcc8e0d896d7f3465697262f5d701e43d40a02fa
William Candillon <wcandillon@gmail.com>
2021-02-26 06:59:37 +0100

    :three:

--- src/rules/unused-exports.ts
+++ src/rules/unused-exports.ts
@@ -1,19 +1,5 @@
 import { ESLintUtils } from "@typescript-eslint/experimental-utils";
-import { Scope } from "@typescript-eslint/experimental-utils/dist/ts-eslint";
-import {
-  Node,
-  isFunctionDeclaration,
-  isBlock,
-  isExpressionStatement,
-  CallExpression,
-  isFunctionTypeNode,
-  getJSDocTags,
-  isArrowFunction,
-  isMethodSignature,
-  isModuleBlock,
-  isSourceFile,
-} from "typescript";
-import analyzeTsConfig, { ExportNameAndLocation } from "ts-unused-exports";
+import analyzeTsConfig from "ts-unused-exports";
 
 export type Options = [];
 export type MessageIds = "UnusedExportsMessage";
@@ -45,22 +31,26 @@ export default createRule<Options, MessageIds>({
     const parserServices = ESLintUtils.getParserServices(context);
     const config = parserServices.program.getCompilerOptions()
       .configFilePath as string;
-    // console.log({ result, context });
     return {
       Program: (node) => {
         const tsNode = parserServices.esTreeNodeToTSNodeMap.get(node);
         const analysis = analyzeTsConfig(config, [tsNode.fileName]);
         const files = Object.values(analysis);
         files.forEach((file) => {
-          file.forEach(({ exportName }) => {
-            context.report({
-              messageId: "UnusedExportsMessage",
-              node,
-              data: {
-                name: exportName,
-              },
-            });
-          });
+          file.forEach(
+            ({ exportName, location: { line, character: column } }) => {
+              context.report({
+                messageId: "UnusedExportsMessage",
+                loc: {
+                  line,
+                  column,
+                },
+                data: {
+                  name: exportName,
+                },
+              });
+            }
+          );
         });
       },
     };
--- tests/fixtures/valid/test1.ts
+++ tests/fixtures/valid/test1.ts
@@ -1,1 +1,3 @@
 const foo = 1;
+
+console.log({ foo });
